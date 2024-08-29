b1db8d0658e2d3f1e7d43aed12c71376b8099a86
dxy <daixianyou@zjut.edu.cn>
2024-08-29 19:11:37 +0800

    feat: init commit

---
+++ src/test/java/cn/comradexy/demo/cache/CustomCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.comradexy.demo.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
