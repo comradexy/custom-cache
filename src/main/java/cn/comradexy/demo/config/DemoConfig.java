f0da2687c83e73356deef9f173bbc94087cfc06d
dxy <daixianyou@zjut.edu.cn>
2024-09-14 20:49:52 +0800

    feat: 更新配置文件

--- src/main/java/cn/comradexy/demo/config/DemoConfig.java
+++ src/main/java/cn/comradexy/demo/config/DemoConfig.java
@@ -7,6 +7,7 @@ import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,12 +23,23 @@ import org.springframework.context.annotation.Configuration;
 public class DemoConfig {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Value("${redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.setCodec(JsonJacksonCodec.INSTANCE);
 
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setPassword(redisPassword);
 
         RedissonClient redissonClient = Redisson.create(config);
 
